6cd8fa22dd7bae2cc41351087eea613a5a40edac
Dustin Graves <dgraves@computer.org>
2004-02-15 04:27:51 +0000

    Autotools update

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+(automake --version) < /dev/null > /dev/null 2>&1 || 
+{
+	echo;
+	echo "You must have automake installed";
+	echo;
+	exit;
+}
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || 
+{
+	echo;
+	echo "You must have autoconf installed";
+	echo;
+	exit;
+}
+
+echo "Generating configuration files for fxcd...."
+echo;
+
+run_cmd()
+{
+	echo executing $* ...;
+	if ! $*; then
+		echo "failed!";
+		exit;
+	fi
+}
+
+run_cmd aclocal;
+run_cmd automake --add-missing --copy;
+run_cmd autoconf;
+
