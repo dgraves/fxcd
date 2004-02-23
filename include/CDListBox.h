fad5ebf412dbae424edc13a9ae92be2c1152823f
Dustin Graves <dgraves@computer.org>
2004-02-23 04:29:02 +0000

    Added CDListBox class to change colors and frame style of the menubutton

---
+++ include/CDListBox.h
@@ -0,0 +1,41 @@
+/* CDListBox.h
+ * Copyright (C) 2001 Dustin Graves <dgraves@computer.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#ifndef _CDLISTBOX_H_
+#define _CDLISTBOX_H_
+
+class CDListBox : public FXListBox
+{
+  FXDECLARE(CDListBox)
+protected:
+  CDListBox() { }
+private:
+  CDListBox(const CDListBox&);
+  CDListBox &operator=(const CDListBox&);
+public:
+  // Constructor
+  CDListBox(FXComposite *p,FXObject* tgt=NULL,FXSelector sel=0,FXuint opts=FRAME_SUNKEN|FRAME_THICK|LISTBOX_NORMAL,FXint x=0,FXint y=0,FXint w=0,FXint h=0,FXint pl=DEFAULT_PAD,FXint pr=DEFAULT_PAD,FXint pt=DEFAULT_PAD,FXint pb=DEFAULT_PAD);
+
+  /// Set window background color
+  virtual void setBackColor(FXColor clr);
+
+  // Change text color
+  virtual void setForeColor(FXColor clr);
+};
+
+#endif
---
+++ src/CDListBox.cpp
@@ -0,0 +1,49 @@
+/* CDListBox.cpp
+ * Copyright (C) 2001 Dustin Graves <dgraves@computer.org>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+#include "fox/fx.h"
+#include "CDListBox.h"
+
+FXIMPLEMENT(CDListBox,FXListBox,NULL,0)
+
+CDListBox::CDListBox(FXComposite *p,FXObject* tgt,FXSelector sel,FXuint opts,FXint x,FXint y,FXint w,FXint h,FXint pl,FXint pr,FXint pt,FXint pb)
+: FXListBox(p,tgt,sel,opts,x,y,w,h,pl,pr,pt,pb)
+{
+  button->setFrameStyle(FRAME_LINE|FRAME_THICK);
+  button->setBaseColor(getTextColor());
+  button->setBorderColor(getTextColor());
+  button->setHiliteColor(getBackColor());
+  button->setTextColor(getTextColor());
+  button->setBackColor(getBackColor());
+}
+
+void CDListBox::setBackColor(FXColor clr)
+{
+  FXListBox::setBackColor(clr);
+  button->setBackColor(clr);
+}
+
+void CDListBox::setForeColor(FXColor clr)
+{
+  setTextColor(clr);
+  button->setBaseColor(clr);
+  button->setBorderColor(clr);
+  button->setShadowColor(clr);
+  button->setHiliteColor(clr);
+  button->setTextColor(clr);
+}
