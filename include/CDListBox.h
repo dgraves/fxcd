2ecb4495ea3d17cb0c3a0a19d6b397ad4bf0d18f
Dustin Graves <dgraves@computer.org>
2004-03-05 01:00:12 +0000

    Updated copyright year

--- include/CDListBox.h
+++ include/CDListBox.h
@@ -1,5 +1,5 @@
 /* CDListBox.h
- * Copyright (C) 2001 Dustin Graves <dgraves@computer.org>
+ * Copyright (C) 2004 Dustin Graves <dgraves@computer.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
--- include/CDWindow.h
+++ include/CDWindow.h
@@ -1,5 +1,5 @@
 /* CDWindow.h
- * Copyright (C) 2001 Dustin Graves <dgraves@computer.org>
+ * Copyright (C) 2001,2004 Dustin Graves <dgraves@computer.org>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
